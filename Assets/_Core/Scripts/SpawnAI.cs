256f95893d01abc66226ed8cce7214edc06d28bb
Esfekia <sertac@gmail.com>
2023-02-03 12:15:05 -0500

    Spawner system initiated, tested with 1 AIobject successfull.

---
+++ Assets/_Core/Scripts/SpawnAI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EmeraldAI.Utility;
+
+
+public class SpawnAI : MonoBehaviour
+{
+    public GameObject characterToSpawn;
+    public float time = 0.0f;
+    public float cooldownDuration = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= cooldownDuration)
+        {
+            time = 0.0f;
+            SpawnAIChar();
+        }
+    }
+
+
+    void SpawnAIChar()
+    {
+        //Spawn our AI using the Emerald Object Pool system
+        Vector3 SpawnPosition = transform.position + transform.forward * 5 + (Random.insideUnitSphere * 2);
+        SpawnPosition.y = transform.position.y;
+        GameObject SpawnedAI = EmeraldAIObjectPool.Spawn(characterToSpawn, SpawnPosition, Quaternion.identity);
+    }
+}
